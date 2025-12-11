311e386330a9805347bc079402800f1ad1df8774
YaKotikTvoy <YaKotikTvoy@yandex.ru>
2025-12-12 00:42:30 +0500

    Change Readme.md, added demonstrate video, and other files

--- Task 2/todolist/src/App.vue
+++ Task 2/todolist/src/App.vue
@@ -1,47 +1,46 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+  import {RouterLink, RouterView} from 'vue-router'
 </script>
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Редактор заметок</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+          aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="navbar-item">
+              <router-link class="nav-link" to="/"><i class="bi-house"></i> Главная</router-link>
+            </li>
+            <li class="navbar-item">
+              <router-link class="nav-link" to="/rate"><i class="bi-star"></i> Оценить</router-link>
+            </li>
+            <li class="navbar-item">
+              <router-link class="nav-link" to="/about"><i class="bi-person-circle"></i> Об авторе</router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
   </header>
-
-  <main>
-    <TheWelcome />
+  <main class="container">
+    <RouterView/>
   </main>
 </template>
 
+<script>
+  export default{
+    data(){
+      return{
+        testmessage : 'А это сообщение сформировано Vue программно', 
+      }
+    }
+  }
+</script>
 <style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
+  
 </style>
